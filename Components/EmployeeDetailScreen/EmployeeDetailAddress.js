c9e00c4bb81e685956b5dc04ea57b672e0c24fbd
winter0227 <59944096+winta0227@users.noreply.github.com>
2023-12-01 22:17:44 +1100

    test

--- Components/EmployeeDetailScreen/EmployeeDetailAddress.js
+++ Components/EmployeeDetailScreen/EmployeeDetailAddress.js
@@ -1,26 +1,30 @@
-import { View, StyleSheet } from "react-native";
-import IndividualText from "./IndividualText";
+/** @format */
+
+import { View, StyleSheet } from "react-native"
+import IndividualText from "./IndividualText"
 
 const EmployeeDetailAddress = (props) => {
-  return (
-    <View style={styles.container}>
-      <IndividualText upperText="Street" lowerText={props.Street} />
-      <IndividualText upperText="City" lowerText={props.City} />
-      <IndividualText upperText="State" lowerText={props.State} />
-      <IndividualText upperText="Zip" lowerText={props.Zip} />
-      <IndividualText upperText="Country" lowerText={props.Country} />
-    </View>
-  );
-};
+   return (
+      <View style={styles.container}>
+         <IndividualText upperText='Street' lowerText={props.Street} />
+         <IndividualText upperText='City' lowerText={props.City} />
+         <IndividualText upperText='State' lowerText={props.State} />
+         <IndividualText upperText='Zip' lowerText={props.Zip} />
+         <IndividualText upperText='Country' lowerText={props.Country} />
+      </View>
+   )
+}
 
-export default EmployeeDetailAddress;
+// idk
+
+export default EmployeeDetailAddress
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#ffffff",
-    marginHorizontal: 12,
-    borderRadius: 16,
-    padding: 12,
-    marginBottom: 12,
-  },
-});
+   container: {
+      backgroundColor: "#ffffff",
+      marginHorizontal: 12,
+      borderRadius: 16,
+      padding: 12,
+      marginBottom: 12,
+   },
+})
